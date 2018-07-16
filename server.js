734eacb89776fe723e449ca49eb73b031757b826
Ecksi <jimmy.dechant@gmail.com>
2018-07-16 13:46:23 -0600

    Add GET endpoint to visit single item by id

--- server.js
+++ server.js
@@ -20,16 +20,20 @@ app.get('/api/v1/items', (request, response) => {
     .catch(error => response.status(500).json({ error }));
 });
 
+app.get('/api/v1/items/:id', (request, response) => {
+  const { id } = request.params;
+
+  database('items').select().where('id', id)
+    .then(items => response.status(200).json(items)[id])
+    .catch(error => response.status(500).json({ error }));
+});
+
 app.post('/api/v1/items', (request, response) => {
   const { name } = request.body;
-
+  
   database('items').insert({ name: name }, 'id')
-    .then(item => {
-      response.status(201).json({ id: item[0] });
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
+    .then(item => response.status(201).json({ id: item[0] }))
+    .catch(error => response.status(500).json({ error }));
 });
 
 app.put('/api/v1/items/:id', (request, response) => {
